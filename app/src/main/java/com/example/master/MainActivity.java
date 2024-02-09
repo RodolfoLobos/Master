4d5ca85a606222f1c5aa75340e5de4e6aa20534c
RodolfoLobos <stevenlovoz3@gmail.com>
2024-02-08 20:56:49 -0600

    Creación de AgendaAmigosSqlite

--- app/src/main/java/com/example/master/MainActivity.java
+++ app/src/main/java/com/example/master/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.master;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtnum1, txtnum2;
+    private TextView lblrespuesta;
+    private RadioGroup optCalculadora;
+    private Button btnCalcular;
+    TabHost tbh;
+    TextView tempVal;
+    Button btn;
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
